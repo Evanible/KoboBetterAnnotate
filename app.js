f6ae37db793262e6eb2d2e74e90ecdee4c5bf21a
Evan <evan@EvanMBA.local>
2024-06-10 21:26:29 +0800

    Remove old index.html and update paths

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // 更新首页路径
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/locales/:lang', (req, res) => {
